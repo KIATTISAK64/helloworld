29ea6c532f7acb0e0388276450d97d5671625fa1
amontep2001 <96216661+KIATTISAK64@users.noreply.github.com>
2021-12-16 11:42:18 +0700

    [°F] to [°C] ro [°C] to [°F]

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemperatureCalculator02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // input
+            // นี้คืองง
+            // textBoxC.Text = "hello";
+            //get input form textbox
+            string input = textBoxC.Text;
+            double C = Convert.ToDouble(input);
+            // [°F] = [°C] × ​9⁄5 + 32.
+            double F = C * 9 / 5 + 32;
+            // show input to textbox
+            textBoxF.Text = F.ToString();
+            // [°C] = (5/9)x([°F]-32)
+
+         
+
+
+        }
+
+        private void Convert2_Click(object sender, EventArgs e)
+        {
+            string input = textBoxF.Text;
+            double F1 = Convert.ToDouble(input);
+            // [°C] = (5/9)x([°F]-32)
+            double C1 = (F1 - 32) * 5 / 9;
+            // show input to textbox
+            textBoxC.Text = C1.ToString();
+            
+
+        }
+    }
+}
